50fae756c20d67806876c4fbed64da9afc624f45
Omar Shareef <omarshdev@gmail.com>
2023-07-05 16:23:39 +0300

    test fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     instrumentationHook: true,
     esmExternals: 'loose',
     serverComponentsExternalPackages: ['mongoose'],
+    outputFileTracingRoot: path.join(__dirname, '../../'),
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
