d360a49ff6050f30645072f827742e67f48a8b65
Omar Shareef <omarshdev@gmail.com>
2023-07-07 12:04:22 +0300

    touching up home styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,47 @@
-import Image from 'next/image';
 import styles from './page.module.css';
 import {PiInfinityDuotone} from 'react-icons/pi';
 import Poster from '@/components/Poster/Poster';
 
-export default function Home() {
+const Home = async () => {
+  const packages = await import('../../package.json');
+  const nextVersion = packages.dependencies.next.replace('^', '');
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        {'<'}Next <PiInfinityDuotone />
-        {'/>'}
-      </h1>
-      <p className={styles.description}>
-        A collection of projects & mini-apps built with Next.js
-      </p>
-
-      <section className={styles.section}>
-        <h2 className={styles.subtitle}>Projects</h2>
-        <ol className={styles.apps}>
-          <Poster poster='/img/next-events.png' link='/next-events'>
-            Next Events
-          </Poster>
-        </ol>
-      </section>
-      <section className={styles.section}>
-        <h2 className={styles.subtitle}>Mini-Apps</h2>
-        <ol className={styles.apps}>
-          <Poster poster='/img/drum-kit.png' link='/mini-apps/drum-kit'>
-            Drum Kit
-          </Poster>
-          <Poster poster='/img/analog-clock.png' link='/mini-apps/analog-clock'>
-            Analog Clock
-          </Poster>
-        </ol>
-      </section>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.title}>
+          {'<'}Next <PiInfinityDuotone />
+          {'/>'}
+        </h1>
+        <p className={styles.description}>
+          Full-stack projects & mini-apps with Next.js {nextVersion}
+        </p>
+      </header>
+      <main className={styles.main}>
+        <section className={styles.section}>
+          <h2 className={styles.subtitle}>Projects</h2>
+          <ol className={styles.apps}>
+            <Poster poster='/img/next-events.png' link='/next-events'>
+              Next Events
+            </Poster>
+          </ol>
+        </section>
+        <section className={styles.section}>
+          <h2 className={styles.subtitle}>Mini-Apps</h2>
+          <ol className={styles.apps}>
+            <Poster poster='/img/drum-kit.png' link='/mini-apps/drum-kit'>
+              Drum Kit
+            </Poster>
+            <Poster
+              poster='/img/analog-clock.png'
+              link='/mini-apps/analog-clock'
+            >
+              Analog Clock
+            </Poster>
+          </ol>
+        </section>
+      </main>
+    </>
   );
-}
+};
+
+export default Home;
