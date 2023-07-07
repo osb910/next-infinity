eef89adddeea51c3a003045bb20cfc05ab5247ba
Omar Shareef <omarshdev@gmail.com>
2023-07-07 10:23:26 +0300

    db connect on featured route

--- src/app/api/events/featured/route.ts
+++ src/app/api/events/featured/route.ts
@@ -1,8 +1,10 @@
 import {NextResponse} from 'next/server';
 import Event from '@/app/next-events/Event.model';
+import {dbConnectNextEvents} from '@/app/next-events/database';
 
 export const GET = async () => {
   try {
+    await dbConnectNextEvents();
     const res = await Event.find({isFeatured: true});
     if (!res) throw new Error('Events not found');
     return NextResponse.json(res, {status: 200});
