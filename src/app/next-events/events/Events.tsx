5fc7a48abfc0a04e11bf22d07530f84aab4f2f2f
Omar Shareef <omarshdev@gmail.com>
2023-07-07 09:56:14 +0300

    refactor events page

---
+++ src/app/next-events/events/Events.tsx
@@ -0,0 +1,42 @@
+import EventsSearch from '@/components/events/EventsSearch';
+import EventItem from '@/components/events/EventItem';
+import styles from './Events.module.css';
+import {IEvent} from '../Event.model';
+import {Metadata} from 'next';
+import {getURL} from '@/utils/path';
+import {dbConnectNextEvents} from '../database';
+
+export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+  title: 'All Events',
+};
+
+const AllEvents = async () => {
+  try {
+    await dbConnectNextEvents();
+    const res = await fetch(getURL('/api/events'), {
+      // next: {revalidate: 1800}, // 30 minutes
+    });
+    const events: IEvent[] = await res.json();
+
+    return (
+      <>
+        <EventsSearch />
+        <ul className={styles.list}>
+          {events.map(item => (
+            <EventItem
+              key={item._id?.toString()}
+              {...item}
+              id={item._id?.toString()}
+            />
+          ))}
+        </ul>
+      </>
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default AllEvents;
