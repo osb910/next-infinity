87e0447502ce722f8415e79ef07bdd783bba19c9
Omar Shareef <omarshdev@gmail.com>
2024-02-10 15:23:25 +0200

    refactor useFullscreen hook

--- src/hooks/useFullscreen.ts
+++ src/hooks/useFullscreen.ts
@@ -1,28 +1,59 @@
 import {useState, useEffect} from 'react';
+import {TIME} from '@/constants/numbers';
 
-const useFullscreen = (elementId: string) => {
+export type FullscreenOptions = {
+  id?: string;
+  useMaximize?: boolean;
+  delay?: number;
+};
+
+const useFullscreen = ({
+  id,
+  useMaximize = false,
+  delay = TIME.goldenSec / 4,
+}: FullscreenOptions) => {
   const [isFullscreen, setIsFullscreen] = useState(false);
+  const [isMaximized, setIsMaximized] = useState(false);
 
   useEffect(() => {
-    const toggleFullscreen = () =>
-      setIsFullscreen(!!document.fullscreenElement);
+    const toggleFullscreen = () => {
+      const isFull = !!document.fullscreenElement;
+      setIsFullscreen(isFull);
+      if (useMaximize && !isFull) {
+        setTimeout(() => setIsMaximized(isFull), delay);
+      }
+    };
     document.addEventListener('fullscreenchange', toggleFullscreen);
 
     return () =>
       document.removeEventListener('fullscreenchange', toggleFullscreen);
-  }, []);
+  }, [useMaximize, delay]);
 
-  if (!document) return [false, () => {}];
+  if (!document)
+    return {
+      isFullscreen: false,
+      isMaximized: false,
+      toggleFullscreen: () => {
+        return;
+      },
+    };
 
-  const element = document?.getElementById(elementId) as HTMLElement;
+  const element = id ? document.getElementById(id) : document.body;
 
   const toggleFullscreen = () => {
-    document.fullscreenElement
-      ? document.exitFullscreen()
-      : element?.requestFullscreen();
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      if (useMaximize) {
+        setIsMaximized(true);
+        setTimeout(() => element?.requestFullscreen(), delay);
+      } else {
+        element?.requestFullscreen();
+      }
+    }
   };
 
-  return [isFullscreen, toggleFullscreen];
+  return {isFullscreen, isMaximized, toggleFullscreen};
 };
 
 export default useFullscreen;
