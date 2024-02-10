e7596421b44582054932cf68e3ffed102604fe49
Omar Shareef <omarshdev@gmail.com>
2024-02-10 18:22:47 +0200

    test root page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ import {dirname, join} from 'path';
 //     .flat(Infinity)
 //     .reduce((i, size) => i + size, 0);
 // };
-
+export const dynamic = 'force-dynamic';
 const Home = async () => {
   try {
     const packages = await import('../../package.json');
@@ -37,7 +37,7 @@ const Home = async () => {
     );
     const dir = dirname(fileURLToPath(import.meta.url));
     console.log({url: import.meta.url, dir, root: join(dir, '..')});
-    console.log(await getFolderNames(join(dir, '..')));
+    console.log(await getFolderNames(join(dir, '..', '..')));
     const appFolder = await getFolderNames('./app');
     console.log({appFolder});
     const projects = appFolder.filter(
