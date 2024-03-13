1584ae784bd71e892acca600090888f63a81871d
Omar Shareef <omarshdev@gmail.com>
2024-03-13 10:51:40 +0200

    add post schema

--- src/services/next-blog/post/post.types.ts
+++ src/services/next-blog/post/post.types.ts
@@ -1,29 +1,30 @@
-import type {Img, P8n} from '@/types';
 import type {Document, Model, Types} from 'mongoose';
+import type {Img, JsonRes} from '@/types';
 import type {IComment} from '../comment';
 
-export interface IPost extends Document {
-  title: string;
-  slug: string;
-  abstract: string;
-  publishedOn: Date;
+export interface PostBase {
   lang: string;
+  title: string;
+  abstract: string;
   file: string;
   content: string;
+}
+
+export interface IPost extends Document, PostBase {
+  slug: string;
+  publishedOn: Date;
+  isFeatured: boolean;
   by?: string;
   author: Types.ObjectId;
   tags: Array<Types.ObjectId>;
   likedBy: Array<Types.ObjectId>;
   comments: Array<Types.ObjectId>;
   images: Array<Img>;
-  l10n?: Array<{
-    lang: string;
-    title: string;
-    abstract: string;
-    file: string;
-    content: string;
-    translatedBy?: string;
-  }>;
+  l10n: Array<
+    PostBase & {
+      translatedBy?: string;
+    }
+  >;
 }
 
 export interface IPostWithComments extends Omit<IPost, 'comments'> {
@@ -40,26 +41,12 @@ export interface IPostStatics {
     limit: string | number | null;
     query?: any | null;
     extraPipeline?: any | null;
-  }) => Promise<
-    {
-      data: Array<IPostWithComments>;
-      status: string;
-      message: string;
-      code: number;
-    } & P8n
-  >;
+  }) => Promise<JsonRes<Array<IPostWithComments>>>;
   getTagsList: () => Promise<Array<{_id: string; count: number}>>;
   getTopPosts: (
     page?: number | string | null,
     limit?: number | string | null
-  ) => Promise<
-    {
-      data: Array<IPost>;
-      status: string;
-      message: string;
-      code: number;
-    } & P8n
-  >;
+  ) => Promise<JsonRes<Array<IPost>>>;
 }
 
 export interface PostModel extends Model<IPost>, IPostStatics {}
