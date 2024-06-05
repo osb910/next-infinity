bde9ac9bbe52a6cb249447474e945bc7a470f624
Omar Shareef <omarshdev@gmail.com>
2024-06-05 23:00:06 +0300

    fix next layout types

--- src/types/next.ts
+++ src/types/next.ts
@@ -10,12 +10,8 @@ export interface PageProps<
   searchParams: K & Record<string, string | string[] | undefined>;
 }
 
-export interface LayoutProps<
-  T = Record<string, any>,
-  K = Record<string, string | string[] | undefined>
-> {
+export interface LayoutProps<T = Record<string, any>> {
   params: T;
-  searchParams: K & Record<string, string | string[] | undefined>;
   children: ReactNode;
 }
 
@@ -29,10 +25,9 @@ export type AppPageSync<
   K = Record<string, string | string[] | undefined>
 > = (props: PageProps<T, K>) => any;
 
-export type Layout<
-  T = Record<string, any>,
-  K = Record<string, string | string[] | undefined>
-> = (props: LayoutProps<T, K>) => Promise<any>;
+export type Layout<T = Record<string, any>> = (
+  props: LayoutProps<T>
+) => Promise<any>;
 
 export type GetMetadata<T extends (...args: any) => any = AppPage> = (
   props: Parameters<T>[0],
