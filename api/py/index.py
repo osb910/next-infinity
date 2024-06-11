165e1e0bf5fb0ec057d4d9b471b1968a761114c0
Omar Shareef <omarshdev@gmail.com>
2024-06-11 19:39:08 +0300

    add flask

---
+++ api/py/index.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import json
+ 
+app = Flask(__name__)
+
+# Define a route that listens to GET requests
+@app.route("/api/hello", methods=['GET'])
+def hello_world():
+    # This function gets called when a GET request hits the /api/py endpoint
+    return json.dumps({'hello': "Hello, World!"})
+
+# Check if the current script is the one being run 'main'
+# if __name__ == '__main__':
+    # Starts the Flask application on port 5328
+    # app.run(port=5328, debug=True)
