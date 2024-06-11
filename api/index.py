9ee7e4b84a31263ce3567ce796c0bdedcc951d70
Omar Shareef <omarshdev@gmail.com>
2024-06-12 01:00:00 +0300

    use api/index for flask

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route that listens to GET requests
 @app.route("/api/hello", methods=['GET'])
 def hello_world():
-    # This function gets called when a GET request hits the /api/py endpoint
+    # This function gets called when a GET request hits the /api/hello endpoint
     return json.dumps({'hello': "Hello, World!"})
 
 # Check if the current script is the one being run 'main'
