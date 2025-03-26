606e1822864a1c00af7f5be07654974a1c9601f2
Omar Shareef <omarshdev@gmail.com>
2025-03-26 16:48:56 +0200

    housekeeping

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,8 +2,8 @@
 
 export const register = async () => {
   try {
-    const {connectDB} = await import('./lib/database');
-    await connectDB();
+    const {nextDBConnect} = await import('./lib/db');
+    await nextDBConnect();
   } catch (err) {
     if (!(err instanceof Error)) return;
     console.error(err.message);
