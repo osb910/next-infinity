33d777cc9aef25ebb26d06a63f599d13284a89d5
Omar Shareef <omarshdev@gmail.com>
2025-03-31 00:57:11 +0200

    mongo bugfix

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,7 +3,7 @@
 export const register = async () => {
   try {
     const {nextDBConnect} = await import('./lib/db');
-    await nextDBConnect();
+    await nextDBConnect({useOldUri: true});
   } catch (err) {
     if (!(err instanceof Error)) return;
     console.error(err.message);
